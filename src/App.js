3567d4f2be315ca6f8dbfb0c2a9bb8208b5ec9ee
Jose Miguel <josleugim@gmail.com>
2022-03-20 12:43:20 -0600

    Added route configuration and home component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom';
+import HomePage from './components/home/HomePage';
 import './App.css';
+import Header from "./components/common/Header";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Header/>
+          <Routes>
+              <Route exact path='/' element={<HomePage/>} />
+              <Route path='*' element={<PageNotFound/>} />
+          </Routes>
+      </div>
   );
 }
 
